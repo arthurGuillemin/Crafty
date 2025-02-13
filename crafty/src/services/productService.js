5c389e70aafe3dfd83359ba23241e1685e15be98
arthurGuillemin <aguillemin291@gmail.com>
2025-02-13 10:55:42 +0100

    fix services front

--- crafty/src/services/productService.js
+++ crafty/src/services/productService.js
@@ -1,31 +1,16 @@
-import supabase from "./supabaseClient";
+// Fetch pour récupérer les détails d'un utilisateur
+export const fetchUserDetails = async (id) => {
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/users/${id}`);
+    
+    if (!response.ok) {
+      throw new Error("Erreur lors de la récupération des informations de l'utilisateur");
+    }
 
-// Récupérer tous les produits
-export const getAllProducts = async () => {
-  const { data, error } = await supabase.from("products").select("*");
-  if (error) throw new Error("Erreur lors de la récupération des produits");
-  return data;
-};
-
-// Récupérer un produit par son ID
-export const getProductById = async (productId) => {
-  const { data, error } = await supabase
-    .from("products")
-    .select("*, users(nom)")
-    .eq("id", productId)
-    .single();
-
-  if (error) throw new Error("Produit non trouvé");
-  return data;
-};
-
-// Récupérer les avis d’un produit
-export const getReviewsByProductId = async (productId) => {
-  const { data, error } = await supabase
-    .from("reviews")
-    .select("*")
-    .eq("product_id", productId);
-
-  if (error) throw new Error("Aucun avis trouvé pour ce produit");
-  return data;
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("fetchUserDetails:", error.message);
+    return null; 
+  }
 };
